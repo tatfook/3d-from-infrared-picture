5890e6f0f1528f4ee20d962990dcebc192d353f8
Mofafa <lsyuan0322@gmail.com>
2017-07-17 12:55:52 +0800

    Update nConti.m

--- CornerDetection/nConti.m
+++ CornerDetection/nConti.m
@@ -1,10 +1,16 @@
 function [ output ] = nConti( A,n )
 %NCONTI 此处显示有关此函数的摘要
-% 判断A中是否有至少n个循环连续的1,A是行向量
+% 判断A中是否有至少n个循环连续的1,A是长度为16的行向量
 m = find(A==0);
-A = [0,A(m(1)+1:end),A(1:m(1))];
-output = max(diff(find(~A)))-1>=n;
-% output = max(diff(find(~A)))-1;
-
-
+if length(m)==0
+    output = true;
+else
+    if m(1) > n
+        output = true;
+    else
+        A = [0,A(m(1)+1:end),A(1:m(1))];
+        output = max(diff(find(~A)))-1>=n;
+        % output = max(diff(find(~A)))-1;
+    end
+end
 end
