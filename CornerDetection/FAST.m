c1e9b631625edb51a27d49471f83231d76864667
Mofafa <lsyuan0322@gmail.com>
2017-07-22 17:25:39 +0800

    Update FAST.m
    fixed bug and add Strongest Selection

--- CornerDetection/FAST.m
+++ CornerDetection/FAST.m
@@ -1,43 +1,76 @@
-function [ x,y ] = FAST( img )
+function [ x,y ] = FAST( img,k )
 %Find corner point detection using FAST-9 algorithm, threshold is 50
 %  I为输入图像
-pic = img;
-[M N D]=size(pic);
-if D==3
-%     pic = toGray(pic);
-    pic=rgb2gray(pic);
-end
-%%
-S = [-3,0;-3,1;-2,2;-1,3;0,3;1,3;2,2;3,1;3,0;3,-1;2,-2;1,-3;0,-3;-1,-3;-2,-2;-3,-1]+[4,4]; %16个点的相对位置
 threshold=50;  %阈值
 nth = 9; %FAST-9
+pic = img;
+[H W D]=size(pic);
+if D==3
+    %     pic = toGray(pic);
+    pic=rgb2gray(pic);
+end
+
+%%
+S = [-3,0;-3,1;-2,2;-1,3;0,3;1,3;2,2;3,1;3,0;3,-1;2,-2;1,-3;0,-3;-1,-3;-2,-2;-3,-1]+[4,4]; %16个点的相对位置
 figure(3);imshow(img);title('FAST角点检测');hold on;
+pic = double(pic);
 tic;
 % points = zeros(M,N);
-s = zeros(M,N);
-for px=4:M-3
-    for py=4:N-3%若I1、I9与中心I0的差均小于阈值，则不是候选点
-        delta1=abs(pic(px-3,py)-pic(px,py))>threshold;
-        delta9=abs(pic(px+3,py)-pic(px,py))>threshold;
-            delta5=abs(pic(px,py+3)-pic(px,py))>threshold;
-            delta13=abs(pic(px,py-3)-pic(px,py))>threshold;
-            if sum([delta1 delta9 delta5 delta13])<3
-                continue;
-            else
-                IS =[];
-                block=pic(px-3:px+3,py-3:py+3);
-                for i = 1:16
-                    IS = [IS,block(S(i,1),S(i,2))];  %圆周上16个点依次的亮度 Intensity set
-                end
-                d = abs(IS-pic(px,py)); %16个点的亮度与中心点的距离
-                lv = d > threshold; %logic value
-                if nConti(lv,nth) == 1
-                    s(px,py) = sum(d);
-%                     points(px,py) = 1;  %储存特征点坐标
+s = zeros(H,W);
+for px=4:H-3
+    for py=4:W-3%若I1、I9与中心I0的差均小于阈值，则不是候选点
+        delta(1)=Sxp(pic(px-3,py)-pic(px,py),threshold); %x1
+        delta(2)=Sxp(pic(px+3,py)-pic(px,py),threshold);  %x9
+        delta(3)=Sxp(pic(px,py+3)-pic(px,py),threshold);  %x5
+        delta(4)=Sxp(pic(px,py-3)-pic(px,py),threshold);  %x13
+        if length(find(delta==0))<3
+            IS =[];
+            block=pic(px-3:px+3,py-3:py+3);
+            for i = 1:16
+                IS = [IS,block(S(i,1),S(i,2))];  %圆周上16个点依次的亮度 Intensity set
+            end
+            d = IS - pic(px,py);
+            lv = d>threshold;
+            if nConti(lv,nth) == 1
+                s(px,py) = sum(lv.*d);
+            else 
+                d = pic(px,py)-IS;
+                lv = d>threshold;
+                if nConti(lv,nth)==1
+                    s(px,py) = sum(lv.*d);
                 end
             end
         end
+%         if length(find(delta==1))>=3 %p is darker possible
+%             IS =[];
+%             block=pic(px-3:px+3,py-3:py+3);
+%             for i = 1:16
+%                 IS = [IS,block(S(i,1),S(i,2))];  %圆周上16个点依次的亮度 Intensity set
+%             end
+%             d = IS - pic(px,py);
+%             lv = d>threshold;
+%             if nConti(lv,nth) == 1
+%                 s(px,py) = sum(lv.*d);
+%             end
+%         else
+%             if length(find(delta==-1))>=3 %P is brighter possible
+%                 IS =[];
+%                 block=pic(px-3:px+3,py-3:py+3);
+%                 for i = 1:16
+%                     IS = [IS,block(S(i,1),S(i,2))];  %圆周上16个点依次的亮度 Intensity set
+%                 end
+%                 d = pic(px,py)-IS;
+%                 lv = d>threshold;
+%                 if nConti(lv,nth) == 1
+%                     s(px,py) = sum(lv.*d);
+%                 end
+%                 
+%             end
+%             
+%         end
     end
+end
+
 
 
 %% Non Maximal Suppression 非极大值抑制 5x5
@@ -45,25 +78,32 @@ for px=4:M-3
 
 for m = 1:length(x)
     area = s(x(m)-2:x(m)+2,y(m)-2:y(m)+2);
-    if x(m) == 347
-        a = 1;
-    end
-    if s(x(m),y(m)) == 0
-        continue;
-    else if length(find(area)) == 1
-%             sum(sum(area)) == 1
-            continue;
-        else
+    %     if x(m) == 347
+    %         a = 1;
+    %     end
+    if s(x(m),y(m)) ~= 0
+        if length(find(area)) ~= 1
+            %             sum(sum(area)) == 1
             mask = zeros(5,5);
-            ms = s(x(m)-2:x(m)+2,y(m)-2:y(m)+2);
-            [mx,my] = find(ms == max(max(ms)));
+            %             ms = s(x(m)-2:x(m)+2,y(m)-2:y(m)+2);
+            [mx,my] = find(area == max(max(area)));
             mask(mx(1),my(1)) = 1;
-%             points(x(m)-2:x(m)+2,y(m)-2:y(m)+2) = mask.*area;
-            s(x(m)-2:x(m)+2,y(m)-2:y(m)+2) = mask.*ms;
+            %             points(x(m)-2:x(m)+2,y(m)-2:y(m)+2) = mask.*area;
+            s(x(m)-2:x(m)+2,y(m)-2:y(m)+2) = mask.*area;
         end
     end
     
 end
+
+
+%% select Strongest point 最强点抑制
+
+[m,n] = size(s);
+[val,index] = sort(s(:),'descend');
+b=zeros(m*n,1);
+b(index(1:k))=val(1:k);
+s = reshape(b,[m,n]);
+
 
 %%
 [y,x] = find(s~=0);  %最终特征点的坐标
